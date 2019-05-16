d78f8f2d1eaecb05cff157441cd05cd472beca98
nille02 <nille0386@googlemail.com>
2019-05-16 14:39:12 +0200

    missing

---
+++ __init__.py
@@ -0,0 +1,3 @@
+import storage
+
+pkgname = "MeinTest"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='MyTests',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Niels',
+    author_email='',
+    description=''
+)
